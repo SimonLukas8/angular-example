11cc7d2fa5ad7d4c173df06da93e7363820ceec1
HuiiBuh <nhaderer1@gmail.com>
2021-05-15 18:19:25 +0200

    Middleware checks for user in db

--- backend/todo_api/middleware.py
+++ backend/todo_api/middleware.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from helpers import decode_user_dict, generate_token
+from helpers import decode_user_dict, generate_token, get_user_from_db
 from settings import SETTINGS
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
@@ -10,17 +10,17 @@ from starlette.responses import Response, JSONResponse
 class AuthenticationMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next) -> Response:
 
-        AuthenticationMiddleware._is_login_url(request)
+        if SETTINGS.spoof_user and not AuthenticationMiddleware._is_login_url(request):
+            auth_header = request.headers.get("authorization")
+            if not auth_header:
+                token = generate_token("default", "1234")
+                request.scope["headers"].append((b"authorization", bytes(token, encoding="UTF_8")))
 
-        if SETTINGS.spoof_user:
-            token = generate_token("default", "1234")
-            request.scope["headers"].append((b"authorization", bytes(token, encoding="UTF_8")))
-
-            request = Request(request.scope, request.receive, request._send)
+                request = Request(request.scope, request.receive, request._send)
 
         is_authorized, error = AuthenticationMiddleware._check_header(request)
 
-        if not is_authorized:
+        if not is_authorized and not AuthenticationMiddleware._is_login_url(request):
             return error
 
         return await call_next(request)
@@ -40,6 +40,10 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
         if "username" not in auth_header or "password" not in auth_header:
             return False, AuthenticationMiddleware._auth_error("`username` or `password field not in header`")
 
+        user = get_user_from_db(auth_header["username"])
+        if not user.password_valid(auth_header["password"]):
+            return False, AuthenticationMiddleware._auth_error("Token invalid")
+
         return True, None
 
     @staticmethod
@@ -48,6 +52,5 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
         return JSONResponse(response, status_code=403)
 
     @staticmethod
-    def _is_login_url( request: Request) -> bool:
-        # print(request.url.path)
-        return True
+    def _is_login_url(request: Request) -> bool:
+        return request.url.path == "/login"
