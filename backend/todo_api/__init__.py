a701dfd20990d7937f693413a9b7c261977f1edd
HuiiBuh <nhaderer1@gmail.com>
2021-05-16 20:29:23 +0200

    Added Cors middleware Closes #1

--- backend/todo_api/__init__.py
+++ backend/todo_api/__init__.py
@@ -1,8 +1,19 @@
-from views import views
 from fastapi import FastAPI
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from todo_api.middleware import AuthenticationMiddleware
+from views import views
 
-app = FastAPI()
+middleware = [
+    Middleware(CORSMiddleware,
+               allow_origin_regex='(https?:\/\/)?(localhost|127\.0\.0\.1)(:[0-9]{2,5})?',
+               allow_headers=["*"],
+               allow_credentials=True,
+               allow_methods=["*"]
+               ),
+    Middleware(AuthenticationMiddleware)
+]
 
-app.add_middleware(AuthenticationMiddleware)
+app = FastAPI(middleware=middleware)
+
 app.include_router(views.router)
